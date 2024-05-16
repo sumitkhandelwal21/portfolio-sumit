38b22b5ccb0aa62fb9234a132d457bbd55de5df5
Sumit-337 <sumit.khandelwal@infoobjects.com>
2024-05-17 00:21:22 +0530

    fourth commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -35,11 +35,10 @@ const NavBar = () => {
         </div>
         
         {nav && 
-        <ul className='flex flex-col justify-center items-center
-            absolute top-0 left-0 w-full h-screen bg-gradient-to-b 
-            from-black to-gray-800 text-gray-400 md:hidden'>
+        <ul className='flex flex-col items-center absolute top-0 right-0 w-3/4 h-screen bg-gradient-to-b 
+            from-black to-gray-800 text-gray-400 md:hidden pt-20' style={{ boxShadow: '5px 0 5px 12px rgba(209, 213, 219, 0.4)' }}>
             {links.map(({id, link}) => (
-            <li key={id} className='px-4 cursor-pointer capitalize py-6 text-3xl'>
+            <li key={id} className='px-4 cursor-pointer capitalize py-6 text-2xl'>
                 <Link to={link} smooth duration={500} onClick={()=>setNav(!nav)}>{link}</Link>
             </li>
             ))}
